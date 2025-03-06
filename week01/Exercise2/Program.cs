7cb2ff09c7bdb2bf04e4f3b1f26c22add53d756d
Julius Miguel Ditan <tmcshs.ditan.julius@gmial.com>
2025-03-07 04:12:10 +0800

    task 1 &2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,80 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letter = "";
+
+        if (grade >= 90)
+            {
+                if (grade > 93)
+                {
+                letter ="A";
+                }
+                else 
+                {
+                    letter ="-A";
+                }
+            }
+        else if (grade >= 80)
+            {
+                if (grade > 87)
+                {
+                letter ="+B";
+                }
+                else if (grade < 87 && grade > 83)
+                {
+                    letter ="B";
+                }
+                else 
+                {
+                    letter ="-B";
+                }
+            }
+        else if (grade >= 70)
+            {
+                if (grade > 77)
+                {
+                letter ="+C";
+                }
+                else if (grade < 77 && grade > 73)
+                {
+                    letter ="C";
+                }
+                else 
+                {
+                    letter ="-C";
+                }
+            }
+        else if (grade >= 60)
+            {
+               if (grade > 67)
+                {
+                letter ="+D";
+                }
+                else if (grade < 67 && grade > 63)
+                {
+                    letter ="D";
+                }
+                else 
+                {
+                    letter ="-D";
+                }
+            }
+         else
+            {
+                letter ="F";
+            }
+
+        Console.WriteLine(letter);
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass");
+        }
     }
 }
